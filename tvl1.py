249173363ff5aff1ff296990a94c0185ef44bc17
Ilya Pavlov <ilya18052000@gmail.com>
2023-05-02 12:35:00 +0300

    till 10th step

--- tvl1.py
+++ tvl1.py
@@ -14,31 +14,57 @@ def midbin(k: int, xs: list) -> list:
 
     return bins
 
+
 def intervals(k, xs) -> list:
     res = []
     b = (max(xs) - min(xs)) / k
     mids = midbin(k, xs)
     leftb = min(xs)
     rightb = leftb + b
-    res.append({{'left', leftb}, {'mid', mids[0]}, {'right', rightb}})
+    res.append([leftb, mids[0], rightb])
     for i in range(1, k, 1):
         leftb = rightb
         rightb = leftb + b
-        res.append({{'left', leftb}, {'mid', mids[i]}, {'right', rightb}})
+        res.append([leftb, mids[i], rightb])
     return res
 
-def introw(xs: list, inters: list) -> list:
-    i = 0
-    res = {}
-    for bin in inters:
-        print(type(bin))
-        # for x in xs:
 
-            # if x > bin[2]:
-            #     break
-            # res[i] += x
-            # i += 1
-    return res
+def freq(array: list, start, fin, relative = False, cumulative = False):
+
+    int = []
+    for el in array:
+        if el < start or el > fin:
+            continue
+        int.append(el)
+
+    print('def freq - Ints:')
+    print(int)
+
+    if not cumulative:
+        ni = len(int)
+
+    else:
+        ni = sum(int)
+
+
+    if not relative:
+        result = ni
+    else:
+        result = ni / len(array)
+
+    return result
+
+
+
+
+
+# def freq_cumulative(n_list: list, length: int, relative = False):
+
+
+
+
+
+
 x = [
     24.44, 18.72, 41.16, 15.22, 6.64, 9.38, 52.47, 64.75, 40.11, 10.69, 40.24,
     53.65, 95.55, 48.30, 47.48, 88.33, 31.89, 17.10, 44.26, 51.47, 11.00, 98.17,
@@ -46,10 +72,10 @@ x = [
     51.88, 37.75, 68.27, 98.33, 52.48, 76.13, 25.13, 40.29, 51.64, 24.84, 63.00,
     90.93, 34.32, 68.75, 58.92, 52.53, 95.99
 ]
-minX:   float = min(x)
-maxX:   float = max(x)
-xSort:  list = sorted(x)
-n:      int = len(x)
+minX: float = min(x)
+maxX: float = max(x)
+xSort: list = sorted(x)
+n: int = len(x)
 
 print("Вариационный ряд:\n{}\n".format(xSort))
 
@@ -60,16 +86,40 @@ print("Количество интервалов разбиения диапаз
 #
 # bin_middle_list = list(midbin(bin_num, xSort))
 ints = intervals(bin_num, xSort)
-print("Разбил диапазон\n{}\n".format(intervals))
+print("Разбил диапазон\n{}\n".format(ints))
 
-introv = introw(xSort, ints)
-print(introv)
 
 # Найти для каждого интервала абсолютные и относительные частоты + абсолютные и относительные накопленные частоты
-absolute = []
 
-# for i in range(bin_num):
+print("\nАбсолютная частота")
+absol_f = []
+for i in range(len(ints)):
+    print("Для интервала ", i+1)
+    freq1 = freq(xSort, ints[i][0], ints[i][2])
+    absol_f.append(freq1)
+    print(freq1)
+
+rel_f = []
+print("\nОтносительная частота")
+for i in range(len(ints)):
+    print("Для интервала ", i+1)
+    freq1 = freq(xSort, ints[i][0], ints[i][2], True)
+    rel_f.append(freq1)
+    print(freq1)
+
+
+print("\nАбсолютная накопленная частота")
+abs_cum_freq = sum(absol_f)
+print(abs_cum_freq)
+
+print("\nОтносительная накопленная частота")
+rel_cum_freq = abs_cum_freq / len(xSort)
+print(rel_cum_freq)
 
 
 
-print("Абсолютная накопленная частота")
+
+
+
+
+# abs_freq(xSort, xSort)
